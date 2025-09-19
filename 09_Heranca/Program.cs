675b420b304e9b090f3a9097443b3caf69c6630d
Pedro Martins <pedromartinss996@gmail.com>
2025-09-18 22:21:03 -0300

    Herança

---
+++ 09_Heranca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Não é possivel instanciar uma classe abstrata
+            //Pessoa obj = new Pessoa
+
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoaFisica.Nome = "Geraldo";
+            pessoaFisica.CPF = "222-444-333-45";
+            pessoaFisica.Telefone = "(14) 14565-5464";
+            pessoaFisica.Imprimir();
+
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoaJuridica.Nome = "Senac Marilia";
+            pessoaJuridica.CNPJ = "99-444-555-76";
+            pessoaJuridica.Telefone = "(113) 23543-1245";
+            pessoaJuridica.Imprimir();
+        }
+    }
+}
