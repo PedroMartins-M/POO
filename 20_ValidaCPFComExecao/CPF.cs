7440bbd25fd041b177630de49be8bda7d1357782
Pedro Martins <pedromartinss996@gmail.com>
2025-10-06 22:17:30 -0300

    Validar CPF com exceção

---
+++ 20_ValidaCPFComExecao/CPF.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _20_ValidaCPFComExcecao
+{
+    public class CPF : IDocumento
+    {
+        public string Numero { get; } // private set serve para impedir alteração externa
+        public CPF(string numero)
+        {
+            // 1 - Eliminar caractres não numéricos
+            this.Numero = Regex.Replace(numero, "[^0-9]", "");
+        }
+
+        // private set serve para impedir alteração externa do codigo abaixo
+        /*public void AlteraCPF(string numero)
+        {
+            //Numero = numero;
+        }*/
+
+        public bool Validar()
+        {
+
+
+            // 2 - Validar se tem 11 digitos
+            if (Numero.Length != 11)
+                //throw new Exception("CPF deve conter 11 digitos");
+                throw new Exception("Todos os números do CPF não podem ser iguais!");
+            
+            // 3 = Validas CPFs com todos os números
+            if (Numero.Distinct().Count() == 1)
+            {
+                Console.WriteLine("CPF inválido! Números repetidos não são permitidos");
+                throw new CPFMesmoNumeroException();
+            }
+
+            //4 - Cálculo do 1º Digito verificador 
+            int digX = CalculaDV(Numero, 9, 10);
+
+            //5 - Cálculo do 2º Digito Verificador 
+            int digY = CalculaDV(Numero, 10, 11);
+
+            //6 - Comparar os dígitos 
+            if (int.Parse(Numero[9].ToString()) == digX && int.Parse(Numero[10].ToString()) == digY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int CalculaDV(string cpf, int qtdeNumeros, int peso)
+        {
+            int soma = 0;
+            char[] cpfVetor = cpf.ToCharArray();
+
+            for (int i = 0; i < qtdeNumeros; i++)
+            {
+                soma += int.Parse(cpfVetor[i].ToString()) * (peso - i);
+            }
+            int resto = soma % 11;
+
+            int digito = 0;
+            if (resto >= 2)
+            {
+                digito = 11 - resto;
+            }
+            return digito;
+        }
+    }
+}
---
+++ 20_ValidaCPFComExecao/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using _20_ValidaCPFComExcecao;
+
+namespace _20_ValidaCPFComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IDocumento documento;
+            while (true)
+            {
+                Console.WriteLine("Informe o CPF");
+                try
+                {
+                    string cpf = Console.ReadLine();
+
+                    documento = new CPF(Console.ReadLine());
+                    break;
+                }
+
+                catch (CPFMesmoNumeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (CPFQtdeDigitosException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"CPF invalido: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Pressione qualquer tecla para continuar.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            }
+
+            if (documento.Validar())
+            {
+                Console.WriteLine("CPF Válido!");
+            }
+            else
+            {
+                Console.WriteLine("CPF Inválido!");
+            }
+
+            /*
+            Console.WriteLine("Informe o CPF");
+            CPF cpf = new CPF(Console.ReadLine());
+                             OU
+            IDocumento documento = new CPF(Console.ReadLine());
+
+            if(cpf.Validar())
+                Console.WriteLine("CPF válido")
+            else
+                Console.WriteLine("CPF inválido")
+
+             */
+        }
+
+    }
+
+}
+
+
+
